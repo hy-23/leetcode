5eaac563271fe23d06d7f418b3795de4a0c57140
hy23 <imharshay@gmail.com>
2022-06-12 18:41:18 +0200

    Inner loop iteration is removed.
    Runtime performance is improved 22 times.
    Memory consumption is reduced by 20 times.
    
    Runtime: 45 ms, faster than 69.24% of C++ online submissions for Longest Palindromic Substring.
    Memory Usage: 7.2 MB, less than 81.94% of C++ online submissions for Longest Palindromic Substring.

--- LongestPalindrome/LongestPalindrome/Source.cpp
+++ LongestPalindrome/LongestPalindrome/Source.cpp
@@ -4,47 +4,90 @@
 
 using namespace std;
 string longestPalindrome(string s) {
-	string ans = "";
-	for (auto i = 0; i < s.size(); i++) { // s = "Bangnal";
-		auto right = i;
-		auto left = i;
-		bool found = true;
-		for (; left < s.size(); left++) {
-			auto answer_len = ans.size();
-			auto remain_len = s.size() - right;
-			if (answer_len >= remain_len) {
-				return ans;
-			}
-			auto dst = left - right; // right = 1; left = 5
-			auto j = 0;
-			auto local_r = right;
-			auto local_l = left;
-			while (j < dst) {
-				if (s[local_r] != s[local_l]) {
-					found = false;
+	int len = s.length(), maxlen = 0, pos = 0;
+	//bool stop = false;
+	string ans;
+	if (len == 1)
+		return s;
+	if (len == 2) {
+		if (s[0] == s[1])
+			return s;
+		ans = s[0];
+		return ans;
+	}
+
+	// find a 3 word palindrome to start off, and then exten on either
+	// side to find a 5 word, 7 word ... palindrome..
+	for (int i = 0; i < len; i++) {
+		//if (stop) {
+			//break;
+		//}
+		// minor adaptation to even word palindrome like 'maam'.
+		if (s[i] == s[i + 1]) // s = kmaam.
+		{
+			int left = i;
+			int right = i + 1;
+			while (left >= 0 && right < len)
+			{
+				if (s[left] != s[right]) {
+					/*
+				* A string of length 'n' can contain two palindromes,
+				* each of length greater than n/2.
+				* So, below condition cannot be used to safely terminate
+				* to terminate the answer.
+				if ((2*maxlen) > len) {
+					stop = true; // break the loop.
+				}
+				*/
 					break;
 				}
-				else {
-					j = j + 2;
-					local_r++;
-					local_l--;
-					found = true;
+				if (s[left] == s[right]) {
+					if (maxlen < right - left + 1) {
+						pos = left;
+						maxlen = right - left + 1;
+					}
+					left--;
+					right++;
 				}
 			}
-			if (found == true) {
-				string temp = s.substr(right, dst + 1);
-				auto palind_len = temp.size();
-				if (ans.size() < palind_len) {
-					ans = temp;
+
+		}
+		int left = i - 1;
+		int right = i + 1;
+		while (left >= 0 && right < len)
+		{
+			if (s[left] != s[right]) {
+				/*
+				* A string of length 'n' can contain two palindromes,
+				* each of length greater than n/2.
+				* So, below condition cannot be used to safely terminate
+				* to terminate the answer.
+				if ((2*maxlen) > len) {
+					stop = true; // break the loop.
 				}
+				*/
+				break;
+			}
+			if (s[left] == s[right]) {
+				if (maxlen < right - left + 1) {
+					pos = left;
+					maxlen = right - left + 1;
+				}
+				left--;
+				right++;
 			}
 		}
+
 	}
+
+	ans = s.substr(pos, maxlen);
+	if (!ans.size())
+		ans.push_back(s[0]);
 	return ans;
 }
 
 int main() {
-	string ans = longestPalindrome("alksmeraaremdeewana");
+	string ans = longestPalindrome("malayalam");
 	cout << ans << endl;
 	return 0;
 }
